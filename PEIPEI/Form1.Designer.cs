8b225dcf05068f4fbc1eddae747d41cbdaef8083
owl <reltsuh1118@gmail.com>
2024-02-09 15:51:24 +0900

    ボタン追加

--- PEIPEI/Form1.Designer.cs
+++ PEIPEI/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            qfile1 = new Button();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // qfile1
+            // 
+            qfile1.Location = new Point(41, 34);
+            qfile1.Name = "qfile1";
+            qfile1.Size = new Size(180, 70);
+            qfile1.TabIndex = 0;
+            qfile1.Text = "共有フォルダ\r\n(192.168.100.250)";
+            qfile1.UseVisualStyleBackColor = true;
+            qfile1.Click += qfile1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(243, 34);
+            button1.Name = "button1";
+            button1.Size = new Size(180, 70);
+            button1.TabIndex = 1;
+            button1.Text = "共有フォルダ\r\n（\\\\NAS303F9E）";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(qfile1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button qfile1;
+        private Button button1;
     }
 }
--- PEIPEI/Form1.cs
+++ PEIPEI/Form1.cs
@@ -6,5 +6,15 @@ namespace PEIPEI
         {
             InitializeComponent();
         }
+
+        private void qfile1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", @"\\192.168.100.250");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", @"\\NAS303F9E");
+        }
     }
 }
